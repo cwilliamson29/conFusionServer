940c96e605053b5b6cb0d84275478c09bd1c4a8e
Chris Williamson <chriswill0629@gmail.com>
2022-08-30 08:46:12 -0400

    testing sessions

--- app.js
+++ app.js
@@ -1,62 +1,127 @@
-var createError = require('http-errors');
-var express = require('express');
-var path = require('path');
-var cookieParser = require('cookie-parser');
-var logger = require('morgan');
+var createError = require("http-errors");
+var express = require("express");
+var path = require("path");
+var cookieParser = require("cookie-parser");
+var logger = require("morgan");
+var session = require("express-session");
+var FileStore = require("session-file-store")(session);
 
-var indexRouter = require('./routes/index');
-var usersRouter = require('./routes/users');
-var dishRouter = require('./routes/dishRouter');
-var promoRouter = require('./routes/promoRouter');
-var leaderRouter = require('./routes/leaderRouter');
+var indexRouter = require("./routes/index");
+var usersRouter = require("./routes/users");
+var dishRouter = require("./routes/dishRouter");
+var promoRouter = require("./routes/promoRouter");
+var leaderRouter = require("./routes/leaderRouter");
 
-const mongoose = require('mongoose');
+const mongoose = require("mongoose");
 
-const Dishes = require('./models/dishes');
+const Dishes = require("./models/dishes");
 
-const url = 'mongodb://localhost:27017/conFusion';
+const url = "mongodb://localhost:27017/conFusion";
 const connect = mongoose.connect(url);
 
-connect.then((db) => {
-    console.log("Connected to server\n*****************");
-}, (err) => {
-    console.log(err)
-});
+connect.then(
+      (db) => {
+            console.log("Connected to server\n*****************");
+      },
+      (err) => {
+            console.log(err);
+      }
+);
 
 var app = express();
 
 // view engine setup
-app.set('views', path.join(__dirname, 'views'));
-app.set('view engine', 'jade');
+app.set("views", path.join(__dirname, "views"));
+app.set("view engine", "jade");
 
-app.use(logger('dev'));
+app.use(logger("dev"));
 app.use(express.json());
-app.use(express.urlencoded({
-    extended: false
-}));
-app.use(cookieParser());
-app.use(express.static(path.join(__dirname, 'public')));
+app.use(
+      express.urlencoded({
+            extended: false,
+      })
+);
+//app.use(cookieParser("12345-67890-09876-54321"));
 
-app.use('/', indexRouter);
-app.use('/users', usersRouter);
-app.use('/dishes', dishRouter);
-app.use('/promotions', promoRouter);
-app.use('/leaders', leaderRouter);
+app.use(
+      session({
+            name: "session-id",
+            secret: "12345-67890-09876-54321",
+            saveUninitialized: false,
+            resave: false,
+            store: new FileStore(),
+      })
+);
+
+function auth(req, res, next) {
+      console.log("\n***header***");
+      console.log(req.signedCookies);
+      console.log("\n***header***");
+      console.log(req.session);
+      console.log("\n***session***");
+
+      if (!req.session.user) {
+            var authHeader = req.headers.authorization;
+            if (!authHeader) {
+                  var err = new Error("not auth");
+
+                  res.setHeader("www-authenticate", "Basic");
+                  err.status = 401;
+                  return next(err);
+            }
+
+            var auth = new Buffer.from(authHeader.split(" ")[1], "base64").toString().split(":");
+
+            var username = auth[0];
+            var password = auth[1];
+
+            if (username === "admin" && password === "password") {
+                  req.session.user = "admin";
+                  next();
+            } else {
+                  var err = new Error("not auth");
+
+                  res.setHeader("www-authenticate", "Basic");
+                  err.status = 401;
+                  return next(err);
+            }
+      } else {
+            if (req.session.user === "admin") {
+                  next();
+            } else {
+                  var err = new Error("not auth");
+
+                  res.setHeader("www-authenticate", "Basic");
+                  err.status = 401;
+                  return next(err);
+            }
+      }
+}
+
+app.use(auth);
+
+app.use(express.static(path.join(__dirname, "public")));
+
+app.use("/", indexRouter);
+app.use("/users", usersRouter);
+app.use("/dishes", dishRouter);
+app.use("/promotions", promoRouter);
+app.use("/leaders", leaderRouter);
 
 // catch 404 and forward to error handler
 app.use(function (req, res, next) {
-    next(createError(404));
+      next(createError(404));
 });
 
 // error handler
 app.use(function (err, req, res, next) {
-    // set locals, only providing error in development
-    res.locals.message = err.message;
-    res.locals.error = req.app.get('env') === 'development' ? err : {};
+      // set locals, only providing error in development
+      res.locals.message = err.message;
+      res.locals.error = req.app.get("env") === "development" ? err : {};
 
-    // render the error page
-    res.status(err.status || 500);
-    res.render('error');
+      // render the error page
+      res.status(err.status || 500);
+      res.render("error");
 });
 
 module.exports = app;
